b5e292e5a4b7b8c098cad18ac6ace979cb46c0d1
roottool <operation.versatile.roottool@gmail.com>
2025-02-02 13:41:39 +0000

    feat: タスクページを新規作成

---
+++ app/tasks/page.tsx
@@ -0,0 +1,15 @@
+import { SecondaryButton } from '@/components/Button'
+
+export default function TasksPage() {
+	return (
+		<main className="flex flex-col gap-12 px-4 py-5">
+			<h2 className="font-bold text-3xl">タスク一覧</h2>
+			<div className="flex gap-2 w-full max-w-md">
+				<div></div>
+				<div className="w-full py-3">
+					<SecondaryButton>Add task</SecondaryButton>
+				</div>
+			</div>
+		</main>
+	)
+}
