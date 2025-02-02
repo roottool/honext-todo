0ed6f03cfdb6d60e484901dd815016ef9dcfccf3
roottool <operation.versatile.roottool@gmail.com>
2025-02-02 09:29:01 +0000

    feat: ログインAPIリクエストにバリデーションを追加

--- app/api/[[...route]]/auth.ts
+++ app/api/[[...route]]/auth.ts
@@ -1,31 +1,37 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { setCookie } from 'hono/cookie'
+import { z } from 'zod'
 
 import admin from '@/infra/firebase/serverApp'
 
 const auth = admin.auth()
-const app = new Hono().post('/login', async (c) => {
-	const { idToken } = await c.req.json<{ idToken: string }>()
-	if (!idToken) {
-		return c.json({ error: 'No ID token provided' }, 400)
-	}
+const app = new Hono().post(
+	'/login',
+	zValidator('json', z.object({ idToken: z.string().jwt() })),
+	async (c) => {
+		const { idToken } = await c.req.json<{ idToken: string }>()
+		if (!idToken) {
+			return c.json({ error: 'No ID token provided' }, 400)
+		}
 
-	try {
-		const decodedToken = await auth.verifyIdToken(idToken)
+		try {
+			const decodedToken = await auth.verifyIdToken(idToken)
 
-		setCookie(c, 'session', idToken, {
-			httpOnly: true,
-			secure: process.env.NODE_ENV === 'production',
-			path: '/',
-			// 有効期間は1日。
-			maxAge: 60 * 60 * 24,
-		})
+			setCookie(c, 'session', idToken, {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === 'production',
+				path: '/',
+				// 有効期間は1日。
+				maxAge: 60 * 60 * 24,
+			})
 
-		return c.json({ uid: decodedToken.uid, email: decodedToken.email })
-	} catch (error) {
-		console.error(error)
-		return c.json({ error: 'Invalid ID token' }, 401)
-	}
-})
+			return c.json({ uid: decodedToken.uid, email: decodedToken.email })
+		} catch (error) {
+			console.error(error)
+			return c.json({ error: 'Invalid ID token' }, 401)
+		}
+	},
+)
 
 export default app
