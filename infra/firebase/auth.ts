b0005e4eba792c04cbaf114881d7225a819d9ea7
roottool <operation.versatile.roottool@gmail.com>
2025-02-02 09:05:57 +0000

    feat: Googleアカウントでのログイン処理を追加

---
+++ infra/firebase/auth.ts
@@ -0,0 +1,12 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+
+import { auth } from './clientApp'
+
+const provider = new GoogleAuthProvider()
+export async function signInWithGoogle() {
+	try {
+		return await signInWithPopup(auth, provider)
+	} catch (error) {
+		throw new Error('Error signing in with Google', { cause: error })
+	}
+}
