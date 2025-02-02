084f3826fd9eb88ef73d245840cea18ac4618f6f
roottool <operation.versatile.roottool@gmail.com>
2025-02-02 13:38:55 +0900

    各種初期設定 (#1)
    * chore: init config files
    
    * chore: More init setting commit
    
    * feat: Firebase設定値の読み込みを追加
    
    * feat: Firebaseセットアップ
    
    * feat: ファビコン追加
    
    * chore: ライセンス記載

---
+++ infra/firebase/clientApp.ts
@@ -0,0 +1,55 @@
+import { type FirebaseApp, getApps, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+import { z } from 'zod'
+
+const clientEnvSchema = z.object({
+	NEXT_PUBLIC_FIREBASE_API_KEY: z.string().min(1),
+	NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string().min(1),
+	NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string().min(1),
+	NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string().min(1),
+	NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string().min(1),
+	NEXT_PUBLIC_FIREBASE_APP_ID: z.string().min(1),
+})
+const validateClientEnv = clientEnvSchema.parse({
+	NEXT_PUBLIC_FIREBASE_API_KEY: process.env['NEXT_PUBLIC_FIREBASE_API_KEY'],
+	NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+		process.env['NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'],
+	NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+		process.env['NEXT_PUBLIC_FIREBASE_PROJECT_ID'],
+	NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+		process.env['NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET'],
+	NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+		process.env['NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'],
+	NEXT_PUBLIC_FIREBASE_APP_ID: process.env['NEXT_PUBLIC_FIREBASE_APP_ID'],
+})
+export const firebaseConfig = {
+	apiKey: validateClientEnv.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: validateClientEnv.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: validateClientEnv.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: validateClientEnv.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: validateClientEnv.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: validateClientEnv.NEXT_PUBLIC_FIREBASE_APP_ID,
+} as const satisfies {
+	apiKey: string
+	authDomain: string
+	projectId: string
+	storageBucket: string
+	messagingSenderId: string
+	appId: string
+}
+
+export const createFirebaseApp = (): FirebaseApp => {
+	const firebaseApp = getApps()
+	if (firebaseApp.length <= 0) {
+		return initializeApp(firebaseConfig)
+	}
+
+	return firebaseApp[0] ?? initializeApp(firebaseConfig)
+}
+
+const firebaseApp = createFirebaseApp()
+export const auth = getAuth(firebaseApp)
+export const db = getFirestore(firebaseApp)
+export const storage = getStorage(firebaseApp)
