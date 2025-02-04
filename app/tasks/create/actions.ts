b9d4619c6d28b508a8db7a392bcad0540ce07135
roottool <operation.versatile.roottool@gmail.com>
2025-02-04 12:51:47 +0000

    feat: タスク新規作成のServer Actionを追加

--- app/tasks/create/actions.ts
+++ app/tasks/create/actions.ts
@@ -1,19 +1,38 @@
 'use server'
 
+import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { parseWithZod } from '@conform-to/zod'
 
 import { taskSchema } from '@/app/tasks/schema'
+import { SESSION_COOKIE_KEY } from '@/infra/cookie'
+import {
+	COLLECTION_NAME,
+	adminDb,
+	verifyIdToken,
+} from '@/infra/firebase/serverApp'
 
-// FIXME: Server function用にESLintの設定を見直す。
-// eslint-disable-next-line @typescript-eslint/require-await
 export async function createTask(_: unknown, formData: FormData) {
 	const submission = parseWithZod(formData, {
 		schema: taskSchema,
 	})
 	if (submission.status !== 'success') {
 		return submission.reply()
+	}
+
+	const idToken = (await cookies()).get(SESSION_COOKIE_KEY)?.value
+	if (!idToken) {
+		return { ok: false, error: 'No token provided' } as const
+	}
+
+	try {
+		const { uid } = await verifyIdToken(idToken)
+		const docRef = adminDb.collection(COLLECTION_NAME)
+		await docRef.add({ uid, isCompleted: false, ...submission.value })
+	} catch (error) {
+		console.error(error)
+		return { ok: false, error: 'The task creation failed' } as const
 	}
 
 	redirect('/tasks')
