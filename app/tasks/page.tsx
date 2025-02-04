30b7cec7a0a1203673b484756496963cbd1871d8
roottool <operation.versatile.roottool@gmail.com>
2025-02-04 12:51:20 +0000

    feat: スタイル調整

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -5,9 +5,9 @@ export default function TasksPage() {
 	return (
 		<main className="flex flex-col gap-12 px-4 py-5">
 			<h2 className="font-bold text-3xl">タスク一覧</h2>
-			<div className="flex flex-col gap-2 w-full max-w-md">
+			<div className="flex flex-col gap-2 w-full">
 				<TaskList />
-				<div className="w-full py-3">
+				<div className="w-full max-w-md py-3">
 					<LinkButton
 						className="w-full"
 						variant="secondary"
