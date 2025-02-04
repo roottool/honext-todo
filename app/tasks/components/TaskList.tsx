afe58eda842af01bf0b630a70c27369e49036a4c
roottool <operation.versatile.roottool@gmail.com>
2025-02-04 14:18:15 +0000

    feat: 取得したファイル一覧を表示

--- app/tasks/components/TaskList.tsx
+++ app/tasks/components/TaskList.tsx
@@ -1,8 +1,10 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { format } from 'date-fns'
 import type { InferResponseType } from 'hono'
 
+import CheckForm from '@/app/tasks/components/CheckForm'
 import { SESSION_COOKIE_KEY } from '@/infra/cookie'
 import { fetcher } from '@/infra/fetcher'
 import { client } from '@/infra/hono/client'
@@ -27,15 +29,25 @@ export default async function TaskList() {
 	})
 	if (!res.ok) {
 		return (
-			<div>
+			<div className="py-2">
 				<p>タスクを取得できませんでした。</p>
 			</div>
 		)
 	}
 
-	return (
-		<div>
-			<p>{res.uid}</p>
+	res.tasks.forEach((task) => {
+		console.log(task)
+		console.log(typeof task.dueDate)
+	})
+	return res.tasks.map(({ id, taskName, dueDate }) => (
+		<div key={id} className="flex gap-2 justify-between py-2">
+			<div>
+				<p className="font-bold">{taskName}</p>
+				<p className="text-tin-gray text-sm">
+					Due: {format(new Date(dueDate), 'yyyy/MM/dd')}
+				</p>
+			</div>
+			<CheckForm />
 		</div>
-	)
+	))
 }
